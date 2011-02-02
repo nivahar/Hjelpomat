b052310d38f090e96000ee845b6816f986649856
Fredrik Fjeld <ffredrik@gmail.com>
2011-02-02 10:48:55 +0100

    First commit

---
+++ includes/foot.inc.php
@@ -0,0 +1,20 @@
+<div id="footer"> 
+ 
+		<div id="footer-content"> 
+	
+			<span class="info"> 
+				Utviklet for TusenFryd AS
+			</span> 
+		
+			<span class="credits"> 
+				av <a href="http://27.fjeldweb.no" target="_blank">Gruppe 27</a>.
+			</span> 
+ 
+		</div> 
+ 
+	</div> 
+	
+ 
+ 
+</body> 
+</html>
---
+++ includes/head.inc.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html> 
+<html lang="no" xml:lang="no"> 
+<head> 
+	
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" /> 
+	<meta name="keywords" content=""> 
+	<meta name="description" content=""> 
+	<title> 
+		Hjelpomat
+	</title> 
+	
+	<link rel="stylesheet" type="text/css" href="/media/css/reset.css" media="screen" /> 
+	<link rel="stylesheet" type="text/css" href="/media/css/forms.css" media="screen" /> 
+	<link rel="stylesheet" type="text/css" href="/media/css/master.css" media="screen" /> 
+	<link rel="stylesheet" type="text/css" href="/media/css/master_1.css" media="screen" /> 
+	
+	<script src="/media/javascript/jquery-1.5.min.js" type="text/javascript"></script> 
+	
+	<link rel="stylesheet" type="text/css" href="/media/css/hjelp.css" /> 
+	<link rel="stylesheet" type="text/css" href="/media/css/hjelp_1.css" />
+ 
+</head> 
