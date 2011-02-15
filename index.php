cb3f4055fe96d1890933c486615cdb64bcd6622c
fredrikfjeld <ffredrik@gmail.com>
2011-02-15 22:33:17 +0100

    Restructuring of index

--- index.php
+++ index.php
@@ -3,23 +3,9 @@
 	require_once('includes/head.inc.php');
 ?>
 		
-		
-			
-			<div id="helpdesk"> 
-				
-				<div class="helpdesk"> 
-					
-					<h2>Her kommer kanskje info?</h2> 
-					<!-- <p class="helpdesk-information"> 
-						Info schminfo
-					</p> -->
-				
-				<div class="text"> 
-					Eller kanskje ikke.
-				</div> 
-							
-			</div> 
-	</div>
+<?php
+	include_once(page($_GET['page']));
+?>
 
 <?php
 	include_once('includes/foot.inc.php');
