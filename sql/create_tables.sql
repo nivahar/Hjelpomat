63625b04dd5004d9b9f10a0bf10a5f788f02bea7
Eirik Viddal Vartdal <eirik.vartdal@gmail.com>
2011-02-16 22:58:44 +0100

    mappe for sql script

---
+++ sql/create_tables.sql
@@ -0,0 +1,33 @@
+/*
+* SQL FILE: create_table
+*
+* Description:
+* This file is to run for building the database for the HJELPOMAT application.
+*
+* Date:
+*/
+
+
+
+/*
+* Tables for user managment:
+*/
+
+--Table: tbl.user
+--Table: tbl.user_level
+
+
+
+/*
+* Tables for cases
+*/
+
+--Table: tbl.case
+--Table: tbl.location
+--Table: 
+
+
+
+/*
+* Tables for logging
+*/
