b16e9cd4118a4f3f401475066cfd95da1297dfce
Fredrik Fjeld <ffredrik@gmail.com>
2011-02-17 00:04:30 +0100

    Rydding i CSS.

--- includes/head.inc.php
+++ includes/head.inc.php
@@ -15,12 +15,9 @@
 	<link rel="stylesheet" type="text/css" href="/media/css/reset.css" media="screen" /> 
 	<link rel="stylesheet" type="text/css" href="/media/css/forms.css" media="screen" /> 
 	<link rel="stylesheet" type="text/css" href="/media/css/master.css" media="screen" /> 
-	<link rel="stylesheet" type="text/css" href="/media/css/master_1.css" media="screen" /> 
+	<link rel="stylesheet" type="text/css" href="/media/css/hjelp.css" media="screen" />
 	
-	<script src="/media/javascript/jquery-1.5.min.js" type="text/javascript"></script> 
-	
-	<link rel="stylesheet" type="text/css" href="/media/css/hjelp.css" /> 
-	<link rel="stylesheet" type="text/css" href="/media/css/hjelp_1.css" />
+	<script src="/media/javascript/jquery-1.5.min.js" type="text/javascript"></script>  
  
 </head> 
 
