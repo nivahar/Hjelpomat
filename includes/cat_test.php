2a7658488d88c62ab0d9e7675cb020d3839fcc92
cathrineskjerping <cathrine.skjerping@gmail.com>
2011-03-10 13:51:46 +0100

    jkhk

---
+++ includes/cat_test.php
@@ -0,0 +1,9 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+include('functions.inc.php');
+print_r(get_department());
+echo nøff;
+?>
