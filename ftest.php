9c5f84bea87550c625ab6be2b4f0bc74f0f1ba8f
Fredrik Fjeld <ffredrik@gmail.com>
2011-03-15 22:11:17 +0100

    Mer testing.

--- ftest.php
+++ ftest.php
@@ -1,8 +1,25 @@
 <?php
 	require_once('includes/functions.inc.php');
+	echo "<strong>Sjekk av avdelinger :</strong><br/>";
 	foreach(get_categories() as $key => $value){
 		echo "ID: $key<br/>";
 		echo "Beskrivelse: ".$value['description']."<br/>";
 		echo "Forelder: ".$value['parent'],"<br/>";
 		echo "<br/>";
 	}
+	
+	//connect_to_tf();
+	$sql =  "SELECT * FROM [tbl.user]";
+	$resultat = mssql_query($sql);
+	$bruker = array();
+	while($rad = mssql_fetch_array($resultat))
+	{
+		$bruker[] = $rad['user_name'];
+	}
+	echo "Listing av brukere: ";
+	print_r($bruker);
+	
+	echo "<br/><strong>Sjekk om bruker eksisterer:<strong/> ";
+	$bruker = user_exists('Eirik');
+	echo $bruker."nei";
+?>
