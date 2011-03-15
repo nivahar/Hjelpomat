ed69e6461acbece9ec33742d6a072cd33d70cf55
Eirik Viddal Vartdal <eirik.vartdal@gmail.com>
2011-03-15 22:42:35 +0100

    Lagt til funksjon get_user_email

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -472,6 +472,20 @@
 
 }
 
+/*
+ * GET MAIL ADDRESS FROM USER ID
+ */
+function get_user_email($user_id){
+    connect_to_tf();
 
+        $sql="SELECT user_email from [tbl.user] where id_user = $user_id;";
+        if(!$data=mssql_query($sql)){
+           return FALSE;
+           exit;
+        }
+          $mail=mssql_fetch_array($data);
+        return $mail['user_email'];
+
+}
 
 ?>
--- includes/functions_eirik.php
+++ includes/functions_eirik.php
@@ -5,5 +5,32 @@
  */
 include('functions.inc.php');
 
-echo save_help_case('Overskrift','1078', '1', 'Dette er en helpdesk sak');
+/*
+ * GET MAIL ADDRESS FROM USER ID
+ */
+function get_user_email($user_id){
+connect_to_tf();
+
+$sql="SELECT user_email from [tbl.user] where id_user = $user_id;";
+if(!$data=mssql_query($sql)){
+    return FALSE;
+    exit;
+}
+$mail=mssql_fetch_array($data);
+return $mail['user_email'];
+
+}
+
+function send_case_email(){
+    
+}
+
+
+
+
+
+
+
+
+
 ?>
